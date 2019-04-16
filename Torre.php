56bfbb3300c13ca0b99da5efc92a08fd17a2bfae
Sebastian Rodriguez <seba.rodriguez.994@gmail.com>
2019-04-16 17:50:11 -0300

    Guarda la fila y columna

--- Torre.php
+++ Torre.php
@@ -1,27 +1,53 @@
 <?php
 	require('PiezaAjedrezInterface.php');
 	
+class Celda {
+
+	private $fila;
+	private $columna;
+	
+	function __construct(string $celda){
+		setCelda(celda);
+	}
+
+	function setCelda(string $celda){
+		$this->columna = (int)$celda[0];
+		$this->fila = (int)$celda[1];
+	}
+
+	public function getFila(){
+		return fila;
+	}
+
+	public function getColumna(){
+		return columna;
+	}
+}
+
+
 class Torre implements PiezaDeAjedrez{
 
-private $posicion;
-private $color;
+	private $posicion;
 
-function __construct($posicion, $color){
-	$this->posicion = $posicion;
-	$this->color = $color;
+	private $color;
+
+	function __construct($posicion, $color){
+		$this->posicion = new Celda($posicion);
+		$this->color = $color;
+	}
+
+
+	public function posicionarEn(string $celda){
+		$this->posicion.setCelda($celda);
+	}
+
+	public function movimientosPosibles(){
+		$posibilidades = [];		
+
+		for($i = 0; $i < 8; $i++){
+					
+		}
+	}
 }
-
-
-public function posicionarEn(string $celda){
-	$this->posicion = celda;
-}
-
-public function movimientosPosibles(){
-
-}
-
-}
-
-?>
 
 
