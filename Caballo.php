6952762783c1260f6b9de20636085e84f2f1c7ae
Camila Ferreyra <40239687+CamilaFerreyra@users.noreply.github.com>
2019-04-16 17:27:38 -0300

    Implemento caballo

--- Caballo.php
+++ Caballo.php
@@ -1,1 +1,24 @@
+<?php
 
+require('PiezaAjedrezInterface.php');
+
+class Caballo implements PiezaAjedrezInterface {
+  public function movimientosPosibles() {
+    $movimientosColumnas =  [2, 2, -2, -2, 1, -1, 1, -1];
+    $movimientosFilas    = [1, -1, 1, -1, 2, 2, -2, -2];
+    
+    $lista = [];
+    for($i = 0; $i < 8; $i++) {
+         $movColumna = $this->columna +  
+    }
+  }
+  
+  public function posicionarEn(string $celda) {
+    
+  }
+  
+  public function __construct(string $celda, string $color) {
+    $this->columna = $celda[0];
+    $this->fila    = $celda[1];
+  }
+}
