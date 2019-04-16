5545f4b0d501f3d0fe8909addb47af92454030b4
Camila Ferreyra <40239687+CamilaFerreyra@users.noreply.github.com>
2019-04-16 17:39:56 -0300

    Prueba caballo

--- Caballo.php
+++ Caballo.php
@@ -4,21 +4,30 @@ require('PiezaAjedrezInterface.php');
 
 class Caballo implements PiezaAjedrezInterface {
   public function movimientosPosibles() {
-    $movimientosColumnas =  [2, 2, -2, -2, 1, -1, 1, -1];
+    $movimientosColumnas = [2, 2, -2, -2, 1, -1, 1, -1];
     $movimientosFilas    = [1, -1, 1, -1, 2, 2, -2, -2];
     
-    $lista = [];
+    $movimientosPosibles = [];
     for($i = 0; $i < 8; $i++) {
-         $movColumna = $this->columna +  
+      $movColumna = $this->columna + movimientosColumnas[i];
+      $movFila    = $this->fila    + movimientosFilas[i];
+      
+      if($movColumna >= 'A' && $movColumna <= 'H'){
+        if($movFila >= 1 && $movFila <= 8){
+          $movimientosPosibles[] = $movColumna . (string)$movFila;
+      } }
     }
+    
+    return $movimientosPosibles;
   }
   
   public function posicionarEn(string $celda) {
-    
+    $this->columna = $celda[0];
+    $this->fila    = (int)$celda[1];
   }
   
   public function __construct(string $celda, string $color) {
     $this->columna = $celda[0];
-    $this->fila    = $celda[1];
+    $this->fila    = (int)$celda[1];
   }
 }
