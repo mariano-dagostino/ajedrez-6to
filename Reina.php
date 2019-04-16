37873776b44551be5e66a6f4769c9a947c2f86ef
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2019-04-16 17:50:18 -0300

    Reina Casi terminada

--- Reina.php
+++ Reina.php
@@ -14,14 +14,25 @@ class Reina implements PiezaDeAjedrez{
 	}
 
 	public function movimientosPosibles(){
-		for($i = 0; $i < 100; $i ++){
-			echo $i;		
+		$equis = $celdax;
+		$ye = $celday;
+
+		for($i = 0; $i < 8; $i ++){
+			imprimir_celda($i , $this->$celday );		
+		}
+
+		for($i = 0; $i < 8; $i ++){
+			imprimir_celda( $this->$celdax , $i);		
 		}
 	}
 
+	public function imprimir_celda(int $x, int $y){
+		echo (chr($x+ 64) ) . $y . "\n";
+	}
+
 	public function posicionarEn(string $p_celda){
-		$celday = $p_celda[1];
-		$celdax = ord($p_celda[0]) - 64;	
+		$this->$celday = $p_celda[1];
+		$this->$celdax = ord($p_celda[0]) - 64;	
 	}
 
 
@@ -31,3 +42,5 @@ const BLANCO = 200;
 
 $reina = new Reina("H3",BLANCO);
 $reina->movimientosPosibles();
+
+$reina->imprimir_celda(4,2);
