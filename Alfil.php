c4eb259f9c07c315f7508c518c6463ce8a483b1c
Iker Canut <iker@localhost.localdomain>
2019-04-18 22:02:19 -0300

    Commit Funcional del Alfil

--- Alfil.php
+++ Alfil.php
@@ -2,7 +2,7 @@
 
 //      18 de Abril de 2019
 //      Iker M. Canut
-
+//
 //      Ejemplo para probar el codigo:
 //              '''
 //              $alfil = new Alfil('E4', 1);
@@ -32,23 +32,22 @@ class Alfil implements PiezaDeAjedrez {
                 );
                 return $listaMovimientosPosibles;
   	}
-        
+
         public function posicionarEn(string $celda){
                 $this->columna = ord($celda[0]);
 		$this->fila = $celda[1];
 	}
 
-        // Son protegidas por si la dama quiere heredar del Alfil y la Torre las funciones
         protected function listaMovimientosEnDireccion(int $modFila, int $modColumna){             
                 $movimientos = array();
                 $auxColumna = $this->columna;
                 $auxFila = $this->fila;
 
-                $auxColumna += $modColumna; 
+                $auxColumna += $modColumna;
                 $auxFila += $modFila;
                 while($auxColumna > 64 && $auxColumna < 73 && $auxFila < 9 && $auxFila > 0){
                         array_push($movimientos, chr($auxColumna) . $auxFila);
-                        $auxColumna += $modColumna; 
+                        $auxColumna += $modColumna;
                         $auxFila += $modFila;
                 }
 
