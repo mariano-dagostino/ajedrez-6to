a16d5f401f39a14b971fa37d17b3c3367da45095
Maxi Nielsen <maxi.a.nielsen@gmail.com>
2019-04-20 23:06:27 -0300

    Ahora si funciona

--- Peon.php
+++ Peon.php
@@ -1,37 +1,44 @@
 <?php
-
 require ('PiezaAjedrezInterface.php');
-
 class Peon implements PiezaDeAjedrez {
-
     protected $color;
     protected $columna;
     protected $fila;
 
-    public function __constructor($posicion, $color){
-
+    public function __construct(string $posicion, $color){
         $this->color = $color;
         $this->columna = $posicion[0];
-        $this->fila = ord($posicion[1]);
-
+        $this->fila = $posicion[1];
     }
-    
+
     public function movimientosPosibles(){
-        if($this->fila == 2){
-            return [(($this->columna).char($this->fila + 1)),(($this->columna).chr($this->fila + 2))];
-        }elseif ($this->fila < 8) {
-            return [(($this->columna).char($this->fila + 1))];
-        }else{
-            return [];
+        $retorno = array();
+        if($this->fila == '8')
+        {
+            return $retorno;
+        }
+        if($this->fila == '2')
+        {
+            $retorno[0] = (($this->columna).($this->fila+2));
+            $retorno[1] = (($this->columna).($this->fila+1));
+            return $retorno;
+        }elseif($this->fila < '8')
+        {
+            $retorno[0] =(($this->columna).($this->fila+1));
+            return $retorno;
         }
     }
-    
-    public function posicionarEn(string $posicion)
-    {
-        $this->columna = $posicion[0];
-        $this->fila = ord($posicion[1]);
+
+    public function posicionarEn(string $posicion){
+        $posPosibles = $this->movimientosPosibles();
+        foreach($posPosibles as $i){
+            if($i[0] == $posicion[0] && $i[1] == $posicion[1]){
+                $this->columna= $posicion[0];
+                $this->fila= $posicion[1];
+
+            }
+        }
     }
 }
 
-$p= new Peon('H2', "BLANCO");
-echo $p->movimientosPosibles();
+?>
