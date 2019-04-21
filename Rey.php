841e6b60561cd1125bb77b03a2a7dc2e70229c01
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2019-04-21 20:09:51 -0300

    Funcion movimientosPosibles arreglada

--- Rey.php
+++ Rey.php
@@ -1,28 +1,44 @@
-//interface PiezaDeAjedrez {
-//  public function movimientosPosibles();
-//  public function posicionarEn(string $celda);
-//}
-
 <?php
 require ('piezaDeAjedrez.php');
+
 class Rey implements PiezaDeAjedrez {
-	public function movimientosPosibles(string $celda);{
-    for ($varX=$celda[0]-1, $contador1 = 0,$nro=0; $contador1 = 3; $contador1++, $varX++, $nro++){
-        if($varX!=10 && $varX!=-1){
-            for ($varY=$celda[1]-1, $contador2=0; $contador2=3 ; $contador2++, $varY++, $nro++){
-                if($varY!=10 && $varY!=-1){
-                    $celdaVar[$varX,$varY];
-                    $celdasPosibles[$nro++]=$celdaVar;
-                }
-            }
-        }
-    }
-    return $celdasPosibles[]; //Devuelve una lista de celdas posibles
+	
+	protected $columna = "D";
+	protected $fila = 1;
+  	protected $color = "Blanco";
+	
+	public function movimientosPosibles()
+	{
+		$movimientos = [];
+		
+		if (1<$this->fila<9)
+		{$movimientos = $this->columna . strval((this->fila)-1);}
+		
+		if (0<$this->fila<7)
+		{$movimientos = $this->columna . strval((this->fila)+1);}
+		
+		if (ord("A")<ord($this->columna)<ord("I"))
+		{$movimientos = chr((ord($this->columna))-1) . strval(this->fila);}
+		
+		if (ord("A")<=ord($this->columna)<ord("G"))
+		{$movimientos = chr((ord($this->columna))+1) . strval(this->fila);}
+		 
+		if (0<$this->fila<7 && ord("A")<=ord($this->columna)<ord("G"))
+		{$movimientos =chr((ord($this->columna))+1) . strval((this->fila)+1);}
+		
+		if (1<$this->fila<9 && ord("A")<ord($this->columna)<ord("I"))
+		{$movimientos =chr((ord($this->columna))-1) . strval((this->fila)-1);}
+		
+		if (0<$this->fila<7 && ord("A")<ord($this->columna)<ord("I"))
+		{$movimientos =chr((ord($this->columna))+1) . strval((this->fila)-1);}
+		
+		if (1<$this->fila<9 && ord("A")<=ord($this->columna)<ord("G"))
+		{$movimientos =chr((ord($this->columna))-1) . strval((this->fila)+1);}
+		
+		return $movimientos; //Devuelve una lista de celdas posibles
 	}
 	public function posicionarEn(string $celda);{
 		$this->fila = $celda[0];
 		$this->columna = $celda[1];
 	}
-	protected $fila;
-	protected $columna;
 }
